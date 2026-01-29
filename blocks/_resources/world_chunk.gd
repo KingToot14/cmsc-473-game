bc67caa75e4ff81cdd80aec12dda289583636837
Jacob Vanluven <46078617+KingToot14@users.noreply.github.com>
2026-01-29 14:40:42 -0500

    Started Autotile System
    I have to create a custom autotile system that can handle chunks. I got the connectivity score done, so now I just need to actually have the connections connect to the right place.

--- blocks/_resources/world_chunk.gd
+++ blocks/_resources/world_chunk.gd
@@ -1,7 +1,103 @@
+@tool
 class_name WorldChunk
 extends Node2D
 
 # --- Variables --- #
-var chunk_pos: Vector2i
+const CONNECTION_MAP = {
+	1: Vector2i(0, 3),
+	2: Vector2i(3, 0),
+	4: Vector2i(1, 0),
+	8: Vector2i(0, 1),
+}
+
+@export var chunk_pos := -Vector2i.ONE
+
+@export_tool_button("Randomize Chunk", "RandomNumberGenerator")
+var _dummy = _randomize_chunk
 
 # --- Functions --- #
+func _ready() -> void:
+	#load_from_data()
+	_randomize_chunk()
+
+#region Tile Management
+func calculate_connections(x: int, y: int) -> int:
+	if x < 0 or x >= TileManager.CHUNK_SIZE or y < 0 or y >= TileManager.CHUNK_SIZE:
+		return 0
+	
+	var chunk = TileManager.get_chunk(chunk_pos.x, chunk_pos.y)
+	
+	# cardinal neighbors
+	var value := 0
+	
+	if TileManager.get_block_in_chunk(chunk, x, y - 1) > 0:
+		value += 1
+	if TileManager.get_block_in_chunk(chunk, x - 1, y) > 0:
+		value += 2
+	if TileManager.get_block_in_chunk(chunk, x + 1, y) > 0:
+		value += 4
+	if TileManager.get_block_in_chunk(chunk, x, y + 1) > 0:
+		value += 8
+	
+	# diagonal neighbors
+	if value & 1 and value & 2 and TileManager.get_block_in_chunk(chunk, x - 1, y - 1) > -1:
+		value += 16
+	if value & 1 and value & 4 and TileManager.get_block_in_chunk(chunk, x + 1, y - 1) > -1:
+		value += 32
+	if value & 8 and value & 2 and TileManager.get_block_in_chunk(chunk, x - 1, y + 1) > -1:
+		value += 64
+	if value & 8 and value & 4 and TileManager.get_block_in_chunk(chunk, x + 1, y + 1) > -1:
+		value += 128
+	
+	return value
+
+#endregion
+
+#region Chunk Management
+func _randomize_chunk() -> void:
+	var blocks: TileMapLayer = $'blocks'
+	
+	clear_chunk()
+	
+	for x in range(TileManager.CHUNK_SIZE):
+		for y in range(TileManager.CHUNK_SIZE):
+			if randf() > 0.6:
+				continue
+			
+			blocks.set_cell(Vector2i(x, y), 2, Vector2i(0, 0))
+			TileManager.set_block(x, y, 1)
+	
+	autotile_block_chunk()
+
+func clear_chunk() -> void:
+	$'walls'.clear()
+	$'blocks'.clear()
+
+func load_from_data() -> void:
+	if chunk_pos == -Vector2i.ONE:
+		return
+	
+	var chunk = TileManager.get_chunk(chunk_pos.x, chunk_pos.y)
+	
+	for x in range(TileManager.CHUNK_SIZE):
+		for y in range(TileManager.CHUNK_SIZE):
+			print(TileManager.get_block_in_chunk(chunk, x, y))
+
+func autotile_block_chunk() -> void:
+	var blocks: TileMapLayer = $'blocks'
+	
+	var chunk = TileManager.get_chunk(chunk_pos.x, chunk_pos.y)
+	
+	for x in range(TileManager.CHUNK_SIZE):
+		for y in range(TileManager.CHUNK_SIZE):
+			var tile := TileManager.get_block_in_chunk(chunk, x, y)
+			var connections := calculate_connections(x, y)
+			
+			print(x, ", ", y, " | ", connections)
+			
+			if connections == 1 or connections == 2 or connections == 4 or connections == 8:
+				print(connections)
+			
+			blocks.set_cell(Vector2i(x, y), tile, CONNECTION_MAP.get(connections, Vector2.ZERO))
+
+#endregion
--- scripts/autoloads/tile_manager.gd
+++ scripts/autoloads/tile_manager.gd
@@ -8,6 +8,15 @@ var chunks: Array[PackedInt32Array]
 var chunk_map: Dictionary[Vector2i, PackedInt32Array] = {}
 
 # --- Functions --- #
+func _ready() -> void:
+	if multiplayer.is_server():
+		add_chunk()
+
+#region Positions
+func chunk_to_world(chunk_x: int, chunk_y: int, x: int, y: int) -> void:
+	pass
+
+#endregion
 
 #region Tile Access
 func _get_tile(world_x: int, world_y: int) -> int:
@@ -18,20 +27,109 @@ func _get_tile(world_x: int, world_y: int) -> int:
 	return chunk[x + y * CHUNK_SIZE]
 
 func get_wall(world_x: int, world_y: int) -> int:
+	# check bounds
+	if world_x < 0 or world_x >= world_size.x or world_y < 0 or world_y >= world_size.y:
+		return 0
+	
 	var tile := _get_tile(world_x, world_y)
 	
 	# wall id is bit 10 - 19
 	return (tile >> 10) & (2**10 - 1)
 
 func get_block(world_x: int, world_y: int) -> int:
+	# check bounds
+	if world_x < 0 or world_x >= world_size.x or world_y < 0 or world_y >= world_size.y:
+		return 0
+	
 	var tile := _get_tile(world_x, world_y)
 	
 	# block id is bit 0 - 9
 	return (tile >> 0) & (2**10 - 1)
 
+func get_wall_in_chunk(chunk: PackedInt32Array, x: int, y: int) -> int:
+	# check bounds
+	if x < 0 or x >= CHUNK_SIZE or y < 0 or y >= CHUNK_SIZE:
+		return 0
+	
+	var tile := chunk[x + y * CHUNK_SIZE]
+	
+	# wall id is bit 10 - 19
+	return (tile >> 10) & (2**10 - 1)
+
+func get_block_in_chunk(chunk: PackedInt32Array, x: int, y: int) -> int:
+	# check bounds
+	if x < 0 or x >= CHUNK_SIZE or y < 0 or y >= CHUNK_SIZE:
+		return 0
+	
+	var tile := chunk[x + y * CHUNK_SIZE]
+	
+	# block id is bit 0 - 9
+	return (tile >> 0) & (2**10 - 1)
+
+func set_wall(world_x: int, world_y: int, wall_id: int) -> void:
+	# check bounds
+	if world_x < 0 or world_x >= world_size.x or world_y < 0 or world_y >= world_size.y:
+		return
+	
+	var chunk := get_chunk_from_world(world_x, world_y)
+	var x := world_x % CHUNK_SIZE
+	var y := world_y % CHUNK_SIZE
+	
+	# clear wall id
+	chunk[x + y * CHUNK_SIZE] &= ~((2**10 - 1) << 10)
+	
+	# set wall id
+	chunk[x + y * CHUNK_SIZE] |= (wall_id << 10)
+
+func set_block(world_x: int, world_y: int, block_id: int) -> void:
+	# check bounds
+	if world_x < 0 or world_x >= world_size.x or world_y < 0 or world_y >= world_size.y:
+		return
+	
+	var chunk := get_chunk_from_world(world_x, world_y)
+	var x := world_x % CHUNK_SIZE
+	var y := world_y % CHUNK_SIZE
+	
+	# clear block id
+	chunk[x + y * CHUNK_SIZE] &= ~((2**10 - 1) << 0)
+	
+	# set block id
+	chunk[x + y * CHUNK_SIZE] |= (block_id << 0)
+
+func set_wall_in_chunk(chunk: PackedInt32Array, x: int, y: int, wall_id: int) -> void:
+	# check bounds
+	if x < 0 or x >= CHUNK_SIZE or y < 0 or y >= CHUNK_SIZE:
+		return
+	
+	# clear wall id
+	chunk[x + y * CHUNK_SIZE] &= ~((2**10 - 1) << 10)
+	
+	# set wall id
+	chunk[x + y * CHUNK_SIZE] |= (wall_id << 10)
+
+func set_block_in_chunk(chunk: PackedInt32Array, x: int, y: int, block_id: int) -> void:
+	# check bounds
+	if x < 0 or x >= CHUNK_SIZE or y < 0 or y >= CHUNK_SIZE:
+		return
+	
+	# clear block id
+	chunk[x + y * CHUNK_SIZE] &= ~((2**10 - 1) << 0)
+	
+	# set block id
+	chunk[x + y * CHUNK_SIZE] |= (block_id << 0)
+
 #endregion
 
 #region Chunk Access
+func add_chunk() -> void:
+	var tiles := PackedInt32Array()
+	
+	for x in range(CHUNK_SIZE):
+		for y in range(CHUNK_SIZE):
+			tiles.append(0)
+	
+	chunks.append(tiles)
+
 func get_chunk(x: int, y: int) -> PackedInt32Array:
 	if multiplayer.is_server():
 		@warning_ignore("integer_division")
