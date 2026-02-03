e6dec6ae5007b74a482979a714ff40c34f8f14de
Jacob Vanluven <46078617+KingToot14@users.noreply.github.com>
2026-02-03 07:29:26 -0500

    Updated Dirt and Grass Tilesets

--- scripts/world_generation/grass_pass.gd
+++ scripts/world_generation/grass_pass.gd
@@ -12,7 +12,9 @@ func perform_pass(gen: WorldGeneration) -> void:
 	var world_size := Globals.world_size
 	
 	# create sliding window
-	var above := TileManager.get_row(0, 0, world_size.x)
+	var prev := TileManager.get_row(0, 0, world_size.x)
+	var curr := TileManager.get_row(0, 1, world_size.x)
+	var next := TileManager.get_row(0, 2, world_size.x)
 	
 	for y in range(1, gen.surface_low + 1):
 		for x in range(1, world_size.x - 1):
@@ -21,8 +23,11 @@ func perform_pass(gen: WorldGeneration) -> void:
 				continue
 			
 			# set grass if there are non-solid tiles above
-			if above[x - 1] <= 0 or above[x] <= 0 or above[x + 1] <= 0:
+			if prev[x - 1] <= 0 or prev[x] <= 0 or prev[x + 1] <= 0 or curr[x - 1] <= 0 or \
+			curr[x + 1] <= 0 or next[x - 1] <= 0 or next[x] <= 0 or next[x + 1] <= 0:
 				TileManager.set_block_unsafe(x, y, 1)
 		
 		# update window
-		above = TileManager.get_row(0, y, world_size.x)
+		prev = curr
+		curr = next
+		next = TileManager.get_row(0, y + 2, world_size.x)
