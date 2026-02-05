de3687c566a4140b4c596b4edec6a8f53abe9a54
Jacob Vanluven <46078617+KingToot14@users.noreply.github.com>
2026-02-05 13:04:00 -0500

    Added Tree Visuals
    Added procedural, deterministic, system for creating tree visuals. This make sure that clients see the same tree with the same height.

--- entities/tile_entities/0_tree/tree_entity.gd
+++ entities/tile_entities/0_tree/tree_entity.gd
@@ -10,4 +10,41 @@ func setup_entity() -> void:
 	rng.seed = data.get(&'branch_seed', 0)
 	
 	# create visuals
+	var sprite: TileMapLayer = $'sprite'
+	var variant: int = data.get(&'variant', 0)
 	
+	# trunk
+	sprite.set_cell(Vector2i(0, 1), variant, Vector2i(0, 1))
+	sprite.set_cell(Vector2i(1, 1), variant, Vector2i(1, 1))
+	sprite.set_cell(Vector2i(0, 0), variant, Vector2i(0, 0))
+	sprite.set_cell(Vector2i(1, 0), variant, Vector2i(1, 0))
+	
+	# main body
+	var height := rng.randi_range(15, 21)
+	var last_branch_l := 0
+	var last_branch_r := 0
+	
+	for y in range(height):
+		# left
+		var branch := 0
+		if y > 0 and y < height - 1 and  rng.randf() < 0.5:
+			branch = rng.randi_range(1, 3)
+			if branch == last_branch_l:
+				branch = 0
+			last_branch_l = branch
+		sprite.set_cell(Vector2i(0, -y - 1), variant, Vector2i(2, branch))
+		
+		# right
+		branch = 0
+		if y > 0 and y < height - 1 and  rng.randf() < 0.5:
+			branch = rng.randi_range(1, 3)
+			if branch == last_branch_r or branch == last_branch_l:
+				branch = 0
+			last_branch_r = branch
+		sprite.set_cell(Vector2i(1, -y - 1), variant, Vector2i(3, branch))
+	
+	# tree top
+	var tree_top := $'tree_top'
+	
+	tree_top.position.y = -(height + 1) * 8.0
+	tree_top.show()
