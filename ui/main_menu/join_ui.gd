e4a0175952fec5cdbf5b7437c1881bca0720aaf4
Jacob Vanluven <46078617+KingToot14@users.noreply.github.com>
2026-02-11 16:08:37 -0500

    Restructured Main Menu
    Updated the main menu with an actual background and title. Also updated the font to be a custom pixel-based one and added a unique join button sprite

--- ui/main_menu/join_ui.gd
+++ ui/main_menu/join_ui.gd
@@ -2,12 +2,34 @@ class_name JoinUi
 extends CanvasLayer
 
 # --- Variables --- #
+@export var disabled_button: Texture2D
+@export var normal_button: Texture2D
+@export var hovered_button: Texture2D
 
+@export var normal_button_color := Color.WHITE
+@export var disabled_button_color := Color.WHITE
+
+var ip_valid := false
+var port_valid := false
+
+var button_hovered := false
 
 # --- Functions --- #
 func _ready() -> void:
+	# multiplayer signals
 	multiplayer.connected_to_server.connect(_on_connected_to_server)
 	multiplayer.connection_failed.connect(_on_connection_failed)
+	
+	# ui signals
+	%'ip_field'.text_changed.connect(_on_ip_changed)
+	%'port_field'.text_changed.connect(_on_port_changed)
+	
+	%'join_button'.mouse_entered.connect(_on_join_mouse_entered)
+	%'join_button'.mouse_exited.connect(_on_join_mouse_exited)
+	
+	# update validity
+	_on_ip_changed(%'ip_field'.text)
+	_on_port_changed(%'port_field'.text)
 
 func _on_connected_to_server() -> void:
 	print("[Wizbowo's Conquest] Client '%s' connected" % multiplayer.get_unique_id())
@@ -21,6 +43,46 @@ func _on_connection_failed() -> void:
 	# re-enable connect options
 	$'connect_options'.show()
 	$'joining_panel'.hide()
+
+#region Connection Info Parsing
+func _on_ip_changed(new_text: String) -> void:
+	ip_valid = new_text.is_valid_ip_address()
+	
+	check_validity()
+
+func _on_port_changed(new_text: String) -> void:
+	port_valid = new_text.is_valid_int() and int(new_text) >= 0
+	
+	check_validity()
+
+func check_validity() -> void:
+	%'join_button'.disabled = not (ip_valid and port_valid)
+	
+	if not (ip_valid and port_valid):
+		%'join_button'.get_node(^'button').texture = disabled_button
+		%'join_button'.get_node(^'title').self_modulate = disabled_button_color
+	elif button_hovered:
+		%'join_button'.get_node(^'button').texture = hovered_button
+		%'join_button'.get_node(^'title').self_modulate = normal_button_color
+	else:
+		%'join_button'.get_node(^'button').texture = normal_button
+		%'join_button'.get_node(^'title').self_modulate = normal_button_color
+
+func _on_join_mouse_entered() -> void:
+	button_hovered = true
+	
+	if not %'join_button'.disabled:
+		%'join_button'.get_node(^'button').texture = hovered_button
+		%'join_button'.get_node(^'title').self_modulate = normal_button_color
+
+func _on_join_mouse_exited() -> void:
+	button_hovered = false
+	
+	if not %'join_button'.disabled:
+		%'join_button'.get_node(^'button').texture = normal_button
+		%'join_button'.get_node(^'title').self_modulate = normal_button_color
+
+#endregion
 
 func create_client() -> Error:
 	# get the fields from LineEdits
