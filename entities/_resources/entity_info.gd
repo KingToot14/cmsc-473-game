b1a14ef56b230bfae4caf31feeb97b8cf9f07d67
Jacob Vanluven <46078617+KingToot14@users.noreply.github.com>
2026-02-16 10:25:21 -0500

    Moved From Entity Paths to Entity Scenes
    Added a new EntityInfo resource that will eventually hold entity spawn rules. Also changed how entity scenes are stored from scene paths to packed scenes.

---
+++ entities/_resources/entity_info.gd
@@ -0,0 +1,10 @@
+class_name EntityInfo
+extends Resource
+
+# --- Variables --- #
+@export var name: String
+@export_multiline var description: String
+
+@export var entity_scene: PackedScene
+
+# --- Functions --- #
