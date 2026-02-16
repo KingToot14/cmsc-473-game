50463041d8dd3cdc2a9178304d34d1f6ad97286f
Jacob Vanluven <46078617+KingToot14@users.noreply.github.com>
2026-02-16 16:40:22 -0500

    Updated Inventory Slot UI
    Added the panel sprite and correct font + outline to the inventory slots.

--- entities/player/inventory_slot.gd
+++ entities/player/inventory_slot.gd
@@ -1,13 +1,17 @@
-extends PanelContainer
-#uncomment this all once we have sprites for the inventory
-#@onready var icon = $Icon #change to the correct name of TextureRect
-#@onready var count_label = $CountLabel #change to correct name of Label
+extends Control
 
-#func update_slot(stack: inventory.ItemStack):
-	#if stack.is_empty():
-		#icon.texture = null
-		#count_label.text = ""
-	#else:
-		#icon.texture = stack.item.texture
-		## Only show numbers if there is more than 1 item
-		#count_label.text = str(stack.count) if stack.count > 1 else ""
+# --- Variables --- #
+@onready var icon = $'item'
+@onready var count_label = $'count_label'
+
+# --- Functions --- #
+func update_slot(stack: Inventory.ItemStack):
+	if stack.is_empty():
+		icon.texture = null
+		count_label.text = ""
+	else:
+		var item: Item = ItemDatabase.get_item(stack.item_id)
+		
+		icon.texture = item.texture
+		# Only show numbers if there is more than 1 item
+		count_label.text = str(stack.count) if stack.count > 1 else ""
