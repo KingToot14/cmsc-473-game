6cab2cc68a2a1a32cd4199a82ea3e491e6a063fa
Jacob Vanluven <46078617+KingToot14@users.noreply.github.com>
2026-02-18 09:37:12 -0500

    Added Basic Tunic Outfit
    Added a very basic tunic outfit with a few pre-set colors. This should be enough to differentiate players for now. I might expand this later on, but I'm a bit sick of drawing outfits for right now.

--- entities/player/outfits/outfit_loader.gd
+++ entities/player/outfits/outfit_loader.gd
@@ -18,42 +18,54 @@ func randomize_outfit() -> void:
 	set_skin_tone(skin_tones.pick_random())
 	
 	if curr_outfit != null:
-		load_outfit(curr_outfit, curr_outfit.get_primary_color(), curr_outfit.get_secondary_color())
+		load_outfit_upper(curr_outfit, curr_outfit.get_primary_color(), curr_outfit.get_secondary_color())
+		load_outfit_lower(curr_outfit, curr_outfit.get_primary_color(), curr_outfit.get_secondary_color())
 	else:
-		load_outfit(curr_outfit, Color.WHITE, Color.WHITE)
+		load_outfit_upper(curr_outfit, Color.WHITE, Color.WHITE)
+		load_outfit_lower(curr_outfit, Color.WHITE, Color.WHITE)
 
 func set_skin_tone(skin_tone: Color) -> void:
 	skin_material.set_shader_parameter(&'primary_color', skin_tone)
 
-func load_outfit(outfit: BaseOutfit, primary_color: Color, secondary_color: Color) -> void:
+func load_outfit_upper(outfit: BaseOutfit, primary_color: Color, secondary_color: Color) -> void:
 	if outfit:
 		$'arm_back_outfit'.texture  = outfit.base_arm_back
-		$'leg_back_outfit'.texture  = outfit.base_leg_back
 		$'body_outfit'.texture      = outfit.base_body
-		$'leg_front_outfit'.texture = outfit.base_leg_front
 		$'head_outfit'.texture      = outfit.base_head
 		$'arm_front_outfit'.texture = outfit.base_arm_front
 		
 		$'arm_back_detail'.texture  = outfit.detail_arm_back
-		$'leg_back_detail'.texture  = outfit.detail_leg_back
 		$'body_detail'.texture      = outfit.detail_body
-		$'leg_front_detail'.texture = outfit.detail_leg_front
 		$'head_detail'.texture      = outfit.detail_head
 		$'arm_front_detail'.texture = outfit.detail_arm_front
 	else:
 		$'arm_back_outfit'.texture  = null
-		$'leg_back_outfit'.texture  = null
 		$'body_outfit'.texture      = null
-		$'leg_front_outfit'.texture = null
 		$'head_outfit'.texture      = null
 		$'arm_front_outfit'.texture = null
 		
 		$'arm_back_detail'.texture  = null
-		$'leg_back_detail'.texture  = null
 		$'body_detail'.texture      = null
-		$'leg_front_detail'.texture = null
 		$'head_detail'.texture      = null
 		$'arm_front_detail'.texture = null
+	
+	# set shaders
+	outfit_material.set_shader_parameter(&'primary_color', primary_color)
+	outfit_material.set_shader_parameter(&'secondary_color', secondary_color)
+
+func load_outfit_lower(outfit: BaseOutfit, primary_color: Color, secondary_color: Color) -> void:
+	if outfit:
+		$'leg_back_outfit'.texture  = outfit.base_leg_back
+		$'leg_front_outfit'.texture = outfit.base_leg_front
+		
+		$'leg_back_detail'.texture  = outfit.detail_leg_back
+		$'leg_front_detail'.texture = outfit.detail_leg_front
+	else:
+		$'leg_back_outfit'.texture  = null
+		$'leg_front_outfit'.texture = null
+		
+		$'leg_back_detail'.texture  = null
+		$'leg_front_detail'.texture = null
 	
 	# set shaders
 	outfit_material.set_shader_parameter(&'primary_color', primary_color)
