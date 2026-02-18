2c78c9b89367b47fb8e1ff46e7627c7214f8d530
Jacob Vanluven <46078617+KingToot14@users.noreply.github.com>
2026-02-18 09:05:06 -0500

    Updated Visuals Layout
    Updated the layout to have 3 layers:
     - Skin: The base skin tone of the character
     - Outfit: The base sprite for the outfits. This doesn't changed based on color
     - Detail: The detail sprite for the outfits. This has a shader that palette swaps.

--- entities/player/outfits/outfit_loader.gd
+++ entities/player/outfits/outfit_loader.gd
@@ -2,9 +2,12 @@ class_name OutfitLoader
 extends Node2D
 
 # --- Variables --- #
-@export var shader_material: ShaderMaterial
+@export var skin_material: ShaderMaterial
+@export var outfit_material: ShaderMaterial
 @export var default_outfit: BaseOutfit
 var curr_outfit: BaseOutfit
+
+@export var skin_tones: Array[Color] = []
 
 # --- Functions --- #
 func _ready() -> void:
@@ -12,25 +15,46 @@ func _ready() -> void:
 	randomize_outfit()
 
 func randomize_outfit() -> void:
+	set_skin_tone(skin_tones.pick_random())
+	
 	if curr_outfit != null:
 		load_outfit(curr_outfit, curr_outfit.get_primary_color(), curr_outfit.get_secondary_color())
 	else:
-		load_outfit(default_outfit, default_outfit.get_primary_color(), default_outfit.get_secondary_color())
+		load_outfit(curr_outfit, Color.WHITE, Color.WHITE)
+
+func set_skin_tone(skin_tone: Color) -> void:
+	skin_material.set_shader_parameter(&'primary_color', skin_tone)
 
 func load_outfit(outfit: BaseOutfit, primary_color: Color, secondary_color: Color) -> void:
-	$'arm_back'.texture  = outfit.base_arm_back
-	$'leg_back'.texture  = outfit.base_leg_back
-	$'body'.texture      = outfit.base_body
-	$'leg_front'.texture = outfit.base_leg_front
-	$'head'.texture      = outfit.base_head
-	$'arm_front'.texture = outfit.base_arm_front
+	if outfit:
+		$'arm_back_outfit'.texture  = outfit.base_arm_back
+		$'leg_back_outfit'.texture  = outfit.base_leg_back
+		$'body_outfit'.texture      = outfit.base_body
+		$'leg_front_outfit'.texture = outfit.base_leg_front
+		$'head_outfit'.texture      = outfit.base_head
+		$'arm_front_outfit'.texture = outfit.base_arm_front
+		
+		$'arm_back_detail'.texture  = outfit.detail_arm_back
+		$'leg_back_detail'.texture  = outfit.detail_leg_back
+		$'body_detail'.texture      = outfit.detail_body
+		$'leg_front_detail'.texture = outfit.detail_leg_front
+		$'head_detail'.texture      = outfit.detail_head
+		$'arm_front_detail'.texture = outfit.detail_arm_front
+	else:
+		$'arm_back_outfit'.texture  = null
+		$'leg_back_outfit'.texture  = null
+		$'body_outfit'.texture      = null
+		$'leg_front_outfit'.texture = null
+		$'head_outfit'.texture      = null
+		$'arm_front_outfit'.texture = null
+		
+		$'arm_back_detail'.texture  = null
+		$'leg_back_detail'.texture  = null
+		$'body_detail'.texture      = null
+		$'leg_front_detail'.texture = null
+		$'head_detail'.texture      = null
+		$'arm_front_detail'.texture = null
 	
-	$'arm_back_detail'.texture  = outfit.detail_arm_back
-	$'leg_back_detail'.texture  = outfit.detail_leg_back
-	$'body_detail'.texture      = outfit.detail_body
-	$'leg_front_detail'.texture = outfit.detail_leg_front
-	$'head_detail'.texture      = outfit.detail_head
-	$'arm_front_detail'.texture = outfit.detail_arm_front
-	
-	shader_material.set_shader_parameter(&'primary_color', primary_color)
-	shader_material.set_shader_parameter(&'secondary_color', secondary_color)
+	# set shaders
+	outfit_material.set_shader_parameter(&'primary_color', primary_color)
+	outfit_material.set_shader_parameter(&'secondary_color', secondary_color)
