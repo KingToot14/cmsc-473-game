ea0848d38b6b14d5b756670d9379c5265c00437e
Jacob Vanluven <46078617+KingToot14@users.noreply.github.com>
2026-02-19 12:43:54 -0500

    Updated Hotbar UI
    Fixed an issue with the hotbar UI appearing off-screen.

--- entities/player/inventory_ui.gd
+++ entities/player/inventory_ui.gd
@@ -1,27 +1,6 @@
+class_name InventoryUI
 extends Control
 
-<<<<<<< Updated upstream
-#Uncomment this all once we have inventory_slot.gd working
-
-#@export var slot_scene: PackedScene #Needs inventory_slot.tscn here in the Inspector
-#@onready var grid = $inventory_grid
-#
-#func setup_ui(player_inventory: inventory):
-	#for child in grid.get_children():
-		#child.queue_free()
-	#
-	#for stack in player_inventory.items:
-		#var new_slot = slot_scene.instantiate()
-		#grid.add_child(new_slot)
-		#new_slot.update_slot(stack)
-	#
-	#player_inventory.inventory_updated.connect(refresh_ui.bind(player_inventory))
-#
-#func refresh_ui(player_inventory: inventory):
-	#var slots = grid.get_children()
-	#for i in range(player_inventory.items.size()):
-		#slots[i].update_slot(player_inventory.items[i])
-=======
 # --- Variables --- #
 @export var slot_scene: PackedScene # Needs inventory_slot.tscn here in the Inspector
 @onready var main_grid = $"inventory_grid"
@@ -53,4 +32,3 @@ func refresh_ui(player_inventory: Inventory):
 	var all_slots = hotbar_grid.get_children() + main_grid.get_children()
 	for i in range(player_inventory.items.size()):
 		all_slots[i].update_slot(player_inventory.items[i])
->>>>>>> Stashed changes
--- entities/player/player_controller.gd
+++ entities/player/player_controller.gd
@@ -10,15 +10,36 @@ const INTERPOLATE_SPEED := 10.0
 		$'input_sync'.set_multiplayer_authority(id)
 		owner_id = id
 		
-		Globals.player = self
+		# set server reference
+		ServerManager.connected_players[id] = self
+		
+		if multiplayer and multiplayer.get_unique_id() == id:
+			Globals.player = self
 
+# - Positioning
 @export var spawn_point: Vector2i
+var center_point: Vector2:
+	get():
+		return $'center'.global_position
 
+# - Movement
 @export var move_speed := 20.0
+@export var jump_power := 180.0
+
+@export var gravity := 980.0
+@export var terminal_velocity := 380.0
 
 var free_cam_mode := false
 var free_cam_pressed := false
-var my_inventory := inventory.new()
+
+# - Inventory
+var my_inventory := Inventory.new()
+
+# - Entity Interest
+var interested_entities: Dictionary[int, bool] = {}
+
+# - Visuals
+var face_direction := 1
 
 var active := true
 
@@ -37,11 +58,14 @@ func _ready() -> void:
 	if owner_id != multiplayer.get_unique_id():
 		$'snapshot_interpolator'.enabled = true
 	else:
-		# update position + control camera
+		# update position
 		position = spawn_point
-<<<<<<< Updated upstream
-		$'camera'.enabled = true
-#	$inventory_ui/inventory_container.setup_ui(my_inventory)
+		
+		# Ensure the sibling hotbar is visible
+		$inventory_ui/hotbar_container.show()
+		
+		# Initialize the UI via the script on the container
+		$inventory_ui/inventory_container.setup_ui(my_inventory)
 
 func _input(event: InputEvent) -> void:
 	if not event.is_action_pressed(&'test_input'):
@@ -54,14 +78,6 @@ func _input(event: InputEvent) -> void:
 	
 	print(mouse_pos, " | ", tile_pos, " | ", global_position, " | ", this_pos)
 	print(TileManager.get_block(tile_pos.x, tile_pos.y))
-=======
-		
-		# Ensure the sibling hotbar is visible
-		$inventory_ui/hotbar_container.show()
-		
-		# Initialize the UI via the script on the container
-		$inventory_ui/inventory_container.setup_ui(my_inventory)
->>>>>>> Stashed changes
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed(&"inventory_toggle"):
@@ -74,6 +90,43 @@ func _unhandled_input(event: InputEvent) -> void:
 		else:
 			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
+#region Animation
+func _process(_delta: float) -> void:
+	# update direction
+	if velocity.x != 0.0 and signf(velocity.x) != face_direction:
+		face_direction = -face_direction
+		
+		for child in $'outfit'.get_children():
+			child.flip_h = not child.flip_h
+	
+	# update animation
+	update_is_on_floor()
+	if is_on_floor():
+		if abs(velocity.x) < 0.10:
+			set_lower_animation(&'idle')
+			set_upper_animation(&'idle')
+		else:
+			set_lower_animation(&'walk')
+			set_upper_animation(&'walk')
+	else:
+		if velocity.y < 0.0:
+			set_lower_animation(&'jump')
+			set_upper_animation(&'jump')
+		else:
+			set_lower_animation(&'fall')
+			set_upper_animation(&'fall')
+
+func set_lower_animation(animation: StringName) -> void:
+	if $'animator_lower'.current_animation != animation:
+		$'animator_lower'.play(animation)
+
+func set_upper_animation(animation: StringName) -> void:
+	if $'animator_upper'.current_animation != animation:
+		$'animator_upper'.play(animation)
+
+#endregion
+
+#region Physics
 func set_free_cam_mode(mode: bool) -> void:
 	free_cam_mode = mode
 	$'shape'.disabled = free_cam_mode
@@ -82,16 +135,16 @@ func _rollback_tick(delta, _tick, _is_fresh) -> void:
 	if active:
 		apply_input(delta)
 
-func apply_input(_delta: float) -> void:
-	var gravity := 980.0
-	var terminal_velocity := 380.0
+func apply_input(delta: float) -> void:
+	# fixes a NetFox bug with is_on_floor()
+	update_is_on_floor()
 	if $'input_sync'.input_jump:
 		if is_on_floor():
-			velocity.y = -400
+			velocity.y = -jump_power
 			pass
 	
-		# gravity
-	velocity.y = clampf(velocity.y + gravity * _delta, -terminal_velocity, terminal_velocity)
+	# gravity
+	velocity.y = clampf(velocity.y + gravity * delta, -terminal_velocity, terminal_velocity)
 	
 	# check free cam
 	if $'input_sync'.input_free_cam:
@@ -105,14 +158,18 @@ func apply_input(_delta: float) -> void:
 	velocity.x = $'input_sync'.input_direction.x * move_speed
 	if free_cam_mode:
 		velocity.y = $'input_sync'.input_direction.y * move_speed
-	else:
-		# apply normal gravity
-		pass
 	
 	# move adjusted to netfox's physics
 	velocity *= NetworkTime.physics_factor
 	move_and_slide()
 	velocity /= NetworkTime.physics_factor
+	
+	# keep inside world boundaries
+	global_position = global_position.clamp(Vector2(4, 4), TileManager.tile_to_world(
+			Globals.world_size.x,
+			Globals.world_size.y
+		) - Vector2(4, 4)
+	)
 
 func update_is_on_floor() -> void:
 	# force an update of is_on_floor after rollbacks occur
@@ -121,9 +178,25 @@ func update_is_on_floor() -> void:
 	move_and_slide()
 	velocity = temp_velocity
 
+#endregion
+
+#region Loading
 func done_initial_load() -> void:
 	active = true
 	$'camera'.enabled = true
+	$'camera'.limit_right  = (Globals.world_size.x) * TileManager.TILE_SIZE
+	$'camera'.limit_bottom = (Globals.world_size.y) * TileManager.TILE_SIZE
 	
 	# hide ui
 	get_tree().current_scene.get_node(^'join_ui').hide()
+
+#endregion
+
+#region Interest
+func add_interest(entity_id: int) -> void:
+	interested_entities[entity_id] = true
+
+func remove_interest(entity_id: int) -> void:
+	interested_entities.erase(entity_id)
+
+#endregion
