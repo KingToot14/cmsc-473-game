7f86c45f0fb405715f0e73da03ecb354cfb523a6
Jacob Vanluven <46078617+KingToot14@users.noreply.github.com>
2026-02-19 08:57:11 -0500

    Updated TileManager Documentation
    Added documentation to all the fields and methods in the TileManager class.

--- scripts/autoloads/tile_manager.gd
+++ scripts/autoloads/tile_manager.gd
@@ -1,18 +1,35 @@
 extends Node2D
 
 # --- Variables --- #
+## The width and height of each chunk in tiles
 const CHUNK_SIZE := 16
+## The area of each chunk in tiles
 const CHUNK_AREA := CHUNK_SIZE * CHUNK_SIZE
+## The widht and height of each tile in world coordinates
 const TILE_SIZE := 8
 
+## A bitmask that isolates the bottom 10 bits
 const MASK_TEN := (1 << 10) - 1
+## A bitmask that isolates the bottom 20 bits
 const MASK_TWENTY := (1 << 20) - 1
+## A bitmask that isolates the wall bits
 const MASK_WALL  := ((1 << 10) - 1) << 10
+## A bitmask that isolates the block bits
 const MASK_BLOCK := ((1 << 10) - 1) << 0
-const MASK_VISUAL := MASK_BLOCK | MASK_BLOCK
+## A bitmask that isolates the wall and block bits
+const MASK_VISUAL := MASK_BLOCK | MASK_WALL
 
+## A flat-packed representation of the world tiles. Each integer represents
+## a single tile in the following format:
+## [br] - Bits 00 - 09: Block ID
+## [br] - Bits 10 - 19: Wall ID
+## [br] - Bits 20 - 31: Unused
 var tiles: PackedInt32Array
+## The width of the game world in tiles.
+## [br]Read from [member Globals.world_size]
 var world_width: int
+## The height of the game world in tiles.
+## [br]Read from [member Globals.world_size]
 var world_height: int
 
 # --- Functions --- #
@@ -25,9 +42,14 @@ func _update_world_size(size: Vector2i) -> void:
 	world_height = size.y
 
 #region Positions
+## Converts local chunk coordinates to global tile coordinates.
+## [br][param chunk_x] and [param chunk_y] represent the chunk in the chunk grid,
+## while [param x] and [param y] represent the position inside the chunk.
+## [br][br][param x] and [param y] should only be values between 0 and 15
 func chunk_to_tile(chunk_x: int, chunk_y: int, x: int = 0, y: int = 0) -> Vector2i:
 	return Vector2i(chunk_x * CHUNK_SIZE + x, chunk_y * CHUNK_SIZE + y)
 
+## Converts global tile coordinates to a chunk in the chunk grid.
 func tile_to_chunk(tile_x: int, tile_y: int) -> Vector2i:
 	@warning_ignore("integer_division")
 	return Vector2i(
@@ -35,6 +57,11 @@ func tile_to_chunk(tile_x: int, tile_y: int) -> Vector2i:
 		clampi(tile_y / CHUNK_SIZE, 0, Globals.world_chunks.y)
 	)
 
+## Converts global tile coordinates to the game's world coordinates.
+## [br]Each tile takes up [code]8.0[/code] world pixels
+## (Determined by [member TileManager.TILE_SIZE])
+## [br][br] If [param center] is [code]true[/code], the resulting position
+## is offset by [code]TileManager.TILE_SIZE / 2.0[/code]
 func tile_to_world(tile_x: int, tile_y: int, center := false) -> Vector2:
 	var offset := 0.0
 	if center:
@@ -42,6 +69,7 @@ func tile_to_world(tile_x: int, tile_y: int, center := false) -> Vector2:
 	
 	return Vector2(tile_x * TILE_SIZE + offset, tile_y * TILE_SIZE + offset)
 
+## Converts the game's world coordinates to a tile coordinate on the grid.
 func world_to_tile(world_x: int, world_y: int) -> Vector2i:
 	@warning_ignore("integer_division")
 	return Vector2i(
@@ -49,6 +77,7 @@ func world_to_tile(world_x: int, world_y: int) -> Vector2i:
 		clampi(world_y / TILE_SIZE, 0, Globals.world_size.y)
 	)
 
+## Converts the game's world coordinates to a chunk in the chunk grid.
 func world_to_chunk(world_x: int, world_y) -> Vector2i:
 	@warning_ignore("integer_division")
 	return Vector2i(
@@ -59,10 +88,14 @@ func world_to_chunk(world_x: int, world_y) -> Vector2i:
 #endregion
 
 #region Tile Access
+## Gets the wall at the given [param x] and [param y] position.
+## [br][br]NOTE: This method does not check bounds in order to improve performance.
+## Use [method get_wall] if you do not know the bounds of [param x] and [param y]
 func get_wall_unsafe(x: int, y: int) -> int:
 	# get wall id (10 to 19)
 	return (tiles[x + y * world_width] >> 10) & MASK_TEN
 
+## Gets the wall at the given [param x] and [param y] position.
 func get_wall(x: int, y: int) -> int:
 	# check bounds
 	if x < 0 or x >= world_width or y < 0 or y >= world_height:
@@ -71,10 +104,14 @@ func get_wall(x: int, y: int) -> int:
 	# get wall id (10 to 19)
 	return (tiles[x + y * world_width] >> 10) & MASK_TEN
 
+## Gets the block at the given [param x] and [param y] position.
+## [br][br]NOTE: This method does not check bounds in order to improve performance.
+## Use [method get_block] if you do not know the bounds of [param x] and [param y]
 func get_block_unsafe(x: int, y: int) -> int:
 	# get block id (0 to 9)
 	return (tiles[x + y * world_width] >> 0) & MASK_TEN
 
+## Gets the block at the given [param x] and [param y] position.
 func get_block(x: int, y: int) -> int:
 	# check bounds
 	if x < 0 or x >= world_width or y < 0 or y >= world_height:
@@ -83,10 +120,16 @@ func get_block(x: int, y: int) -> int:
 	# get block id (0 to 9)
 	return (tiles[x + y * world_width] >> 0) & MASK_TEN
 
+## Gets the block and wall at the given [param x] and [param y] position.
+## [br]This is returned in the bit-packed format, so this should only be used internally
+## [br][br]NOTE: This method does not check bounds in order to improve performance.
+## Use [method get_wall] if you do not know the bounds of [param x] and [param y]
 func get_visual_unsafe(x: int, y: int) -> int:
 	# get wall id (10 to 19)
 	return (tiles[x + y * world_width] >> 0) & MASK_TWENTY
 
+## Gets the block and wall at the given [param x] and [param y] position.
+## [br]This is returned in the bit-packed format, so this should only be used internally
 func get_visual(x: int, y: int) -> int:
 	# check bounds
 	if x < 0 or x >= world_width or y < 0 or y >= world_height:
@@ -95,12 +138,16 @@ func get_visual(x: int, y: int) -> int:
 	# get wall id (10 to 19)
 	return (tiles[x + y * world_width] >> 0) & MASK_TWENTY
 
+## Sets the wall at the given [param x] and [param y] position to [param wall_id].
+## [br][br]NOTE: This method does not check bounds in order to improve performance.
+## Use [method set_wall] if you do not know the bounds of [param x] and [param y]
 func set_wall_unsafe(x: int, y: int, wall_id: int) -> void:
 	# clear tile id
 	var idx = x + y * world_width
 	tiles[idx] &= ~MASK_WALL
 	tiles[idx] |= (wall_id << 10)
 
+## Sets the wall at the given [param x] and [param y] position to [param wall_id].
 func set_wall(x: int, y: int, wall_id: int) -> void:
 	# check bounds
 	if x < 0 or x >= world_width or y < 0 or y >= world_height:
@@ -111,12 +158,16 @@ func set_wall(x: int, y: int, wall_id: int) -> void:
 	tiles[idx] &= ~MASK_WALL
 	tiles[idx] |= (wall_id << 10)
 
+## Sets the block at the given [param x] and [param y] position to [param block_id].
+## [br][br]NOTE: This method does not check bounds in order to improve performance.
+## Use [method set_block] if you do not know the bounds of [param x] and [param y]
 func set_block_unsafe(x: int, y: int, block_id: int) -> void:
 	# clear tile id
 	var idx = x + y * world_width
 	tiles[idx] &= ~MASK_BLOCK
 	tiles[idx] |= (block_id << 0)
 
+## Sets the block at the given [param x] and [param y] position to [param block_id].
 func set_block(x: int, y: int, block_id: int) -> void:
 	# check bounds
 	if x < 0 or x >= world_width or y < 0 or y >= world_height:
@@ -127,6 +178,9 @@ func set_block(x: int, y: int, block_id: int) -> void:
 	tiles[idx] &= ~MASK_BLOCK
 	tiles[idx] |= (block_id << 0)
 
+## Returns the [param y]th row of blocks starting at [param start_x],
+## ending at [code]start_x + width[/code]. If the row goes out of bounds in
+## either direction, uses [param default] instead.
 func get_block_row(start_x: int, y: int, width: int, default := 1) -> PackedInt32Array:
 	var world_w := world_width
 	var world_h := world_height
@@ -161,6 +215,9 @@ func get_block_row(start_x: int, y: int, width: int, default := 1) -> PackedInt3
 	
 	return row
 
+## Returns the [param y]th row of walls starting at [param start_x],
+## ending at [code]start_x + width[/code]. If the row goes out of bounds in
+## either direction, uses [param default] instead.
 func get_wall_row(start_x: int, y: int, width: int, default := 1) -> PackedInt32Array:
 	var world_w := world_width
 	var world_h := world_height
@@ -195,6 +252,9 @@ func get_wall_row(start_x: int, y: int, width: int, default := 1) -> PackedInt32
 	
 	return row
 
+## Returns the [param y]th row of blocks and walls starting at [param start_x],
+## ending at [code]start_x + width[/code]. If the row goes out of bounds in
+## either direction, uses [param default] instead.
 func get_visual_row(start_x: int, y: int, width: int, default := 1) -> PackedInt32Array:
 	var world_w := world_width
 	var world_h := world_height
@@ -232,15 +292,29 @@ func get_visual_row(start_x: int, y: int, width: int, default := 1) -> PackedInt
 #endregion
 
 #region Safe Interactions
+## Attempts to destroy the block at the given [param x] and [param y] position.
+## [br][br]Returns [code]true[/code] if the interaction should be consumed. This is
+## only [code]false[/code] when there is no block at the given position.
 func destroy_block(x: int, y: int) -> bool:
-	# do not process if no block exists
-	if not TileManager.get_block(x, y):
-		return false
-	
 	# check bounds (consume interaction)
 	if x < 0 or x >= world_width:
 		return true
 	if y < 0 or y >= world_height:
+		return true
+	
+	# do not process if no block exists
+	if not TileManager.get_block_unsafe(x, y):
+		return false
+	
+	# check for reserved tiles using physics query
+	var direct_space: PhysicsDirectSpaceState2D = get_world_2d().direct_space_state
+	var query := PhysicsShapeQueryParameters2D.new()
+	query.shape = RectangleShape2D.new()
+	query.shape.size = Vector2(8.0, 8.0)
+	query.transform.origin = tile_to_world(x, y, true)
+	query.collision_mask = 0b01000000	# Only collides with Tile layer
+	
+	if not direct_space.intersect_shape(query, 1).is_empty():
 		return true
 	
 	# TODO: Check player's current tool
@@ -261,16 +335,19 @@ func destroy_block(x: int, y: int) -> bool:
 	
 	return true
 
+## Attempts to destroy the wall at the given [param x] and [param y] position.
+## [br][br]Returns [code]true[/code] if the interaction should be consumed. This is
+## only [code]false[/code] when there is no wall at the given position.
 func destroy_wall(x: int, y: int) -> bool:
-	# do not process if no block exists
-	if not TileManager.get_wall(x, y):
-		return false
-	
 	# check bounds (consume interaction)
 	if x < 0 or x >= world_width:
 		return true
 	if y < 0 or y >= world_height:
 		return true
+	
+	# do not process if no block exists
+	if not TileManager.get_wall_unsafe(x, y):
+		return false
 	
 	# TODO: Check player's current tool
 	
@@ -290,16 +367,19 @@ func destroy_wall(x: int, y: int) -> bool:
 	
 	return true
 
+## Attempts to place [param block_id] at the given [param x] and [param y] position.
+## [br][br]Returns [code]true[/code] if the space is empty. Returns [code]false[/code]
+## if a block already exists at [param x] and [param y].
 func place_block(x: int, y: int, block_id: int) -> bool:
-	# do not process if block exists
-	if TileManager.get_block(x, y):
-		return false
-	
 	# check bounds (consume interaction)
 	if x < 0 or x >= world_width:
 		return true
 	if y < 0 or y >= world_height:
 		return true
+	
+	# do not process if block exists
+	if TileManager.get_block_unsafe(x, y):
+		return false
 	
 	# TODO: Check player's current item
 	
@@ -314,7 +394,7 @@ func place_block(x: int, y: int, block_id: int) -> bool:
 	query.shape = RectangleShape2D.new()
 	query.shape.size = Vector2(8.0, 8.0)
 	query.transform.origin = tile_to_world(x, y, true)
-	query.collision_mask = 0b01001010
+	query.collision_mask = 0b01001010	# Collides with players, enemies, and tiles
 	
 	if not direct_space.intersect_shape(query, 1).is_empty():
 		return true
@@ -332,16 +412,19 @@ func place_block(x: int, y: int, block_id: int) -> bool:
 	
 	return true
 
+## Attempts to place [param wall_id] at the given [param x] and [param y] position.
+## [br][br]Returns [code]true[/code] if the space is empty. Returns [code]false[/code]
+## if a wall already exists at [param x] and [param y].
 func place_wall(x: int, y: int, wall_id: int) -> bool:
-	# do not process if block exists
-	if TileManager.get_wall(x, y):
-		return false
-	
 	# check bounds (consume interaction)
 	if x < 0 or x >= world_width:
 		return true
 	if y < 0 or y >= world_height:
 		return true
+	
+	# do not process if block exists
+	if TileManager.get_wall_unsafe(x, y):
+		return false
 	
 	# TODO: Check player's current item
 	
@@ -351,7 +434,7 @@ func place_wall(x: int, y: int, wall_id: int) -> bool:
 		return true
 	
 	# set tile to wall
-	TileManager.set_block_unsafe(x, y, wall_id)
+	TileManager.set_wall_unsafe(x, y, wall_id)
 	Globals.world_map.update_tile(x, y)
 	
 	# sync to server
@@ -363,6 +446,11 @@ func place_wall(x: int, y: int, wall_id: int) -> bool:
 	
 	return true
 
+## Determines if the position [param x] and [param y] is valid for placing a block.
+## [br] This is [code]true[/code] in two scenarios: 1) A wall is located at [param x]
+## and [param y] 2) A block exists in a neighboring cardinal direction.
+## [br][br]Returns [code]false[/code] if a block already exists, or if neither of the
+## above cases are true.
 func is_block_placement_valid(x: int, y: int) -> bool:
 	# check self block
 	if TileManager.get_block_unsafe(x, y):
@@ -384,6 +472,10 @@ func is_block_placement_valid(x: int, y: int) -> bool:
 	
 	return false
 
+## Determines if the position [param x] and [param y] is valid for placing a wall.
+## [br] This is [code]true[/code] when there is a block or wall in any cardinal direction
+## [br][br]Returns [code]false[/code] if a wall already exists, or if the above
+## case is false
 func is_wall_placement_valid(x: int, y: int) -> bool:
 	# check self block
 	if TileManager.get_block_unsafe(x, y):
@@ -400,6 +492,10 @@ func is_wall_placement_valid(x: int, y: int) -> bool:
 		return true
 	
 	return false
+
+@rpc('any_peer', 'call_remote', 'reliable')
+func send_destroy_block() -> void:
+	pass
 
 @rpc('any_peer', 'call_remote', 'reliable')
 func update_tile_state(x: int, y: int, tile: int, wepaon_id: int, player_id: int) -> void:
@@ -433,6 +529,8 @@ func receive_tile_state(x: int, y: int, tile: int) -> void:
 #endregion
 
 #region Chunk Access
+## Loads the initial tile array to match the world size using
+## [member world_width] and [member world_height]
 func load_chunks() -> void:
 	tiles = []
 	tiles.resize(world_width * world_height)
@@ -440,6 +538,9 @@ func load_chunks() -> void:
 #endregion
 
 #region Multiplayer
+## Packs a range of chunks into a ZSTD-compressed binary format for network sending.
+## [br]NOTE: This is a wrapper around [method pack_region] to use chunk coordinates
+## instead of tile coordinates
 func pack_chunks(start_x: int, start_y: int, width: int, height: int) -> PackedByteArray:
 	return pack_region(
 		start_x * TileManager.CHUNK_SIZE,
@@ -448,6 +549,8 @@ func pack_chunks(start_x: int, start_y: int, width: int, height: int) -> PackedB
 		height  * TileManager.CHUNK_SIZE
 	)
 
+
+## Packs a range of tiles into a ZSTD-compressed binary format for network sending.
 func pack_region(start_x: int, start_y: int, width: int, height: int) -> PackedByteArray:
 	var packed = PackedByteArray()
 	#var offset := 0
@@ -459,6 +562,9 @@ func pack_region(start_x: int, start_y: int, width: int, height: int) -> PackedB
 	
 	return packed.compress(FileAccess.COMPRESSION_ZSTD)
 
+## Updates the tiles from [param start_x] and [param start_y] to [code]start_x + width[/code]
+## and [code]start_y + height[/code].
+## [br]Uses a dirty update system to only update changed regions.
 func load_region(data: PackedInt32Array, start_x: int, start_y: int, width: int, height: int) -> void:
 	var processed := 0
 	
