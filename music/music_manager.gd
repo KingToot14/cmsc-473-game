5670dc46e4027ae8cbda796175ea56b2166bd04b
Jacob Vanluven <46078617+KingToot14@users.noreply.github.com>
2026-02-25 12:26:45 -0500

    Started Implementing Music
    Added a global AudioStreamInteractive to allow cross fading between tracks. Also started the title theme by default.
    
    Added an option to disable music during testing (this is especially important for multiple instance testing)

---
+++ music/music_manager.gd
@@ -0,0 +1,68 @@
+class_name MusicManager
+extends AudioStreamPlayer
+
+# --- Enums --- #
+enum Area {
+	TITLE_SCREEN,
+	FOREST_DAY,
+	FOREST_NIGHT,
+	WINTER_DAY,
+	WINTER_NIGHT,
+	UNDERGROUND,
+	CAVERN,
+	DUNGEON
+}
+
+# --- Variables --- #
+const AREA_TRACKS: Dictionary[Area, Array] = {
+	Area.TITLE_SCREEN: [
+		0, # res://music/menus/title.ogg
+	],
+	Area.FOREST_DAY: [
+		1, # res://music/forest/forest_day_1.ogg,
+		2, # res://music/forest/forest_day_2.ogg
+	],
+	Area.FOREST_NIGHT: [
+		0,
+	],
+	Area.WINTER_DAY: [
+		3, # res://music/winter/winter_day_1.ogg,
+		4, # res://music/winter/winter_day_2.ogg
+	],
+	Area.WINTER_NIGHT: [
+		0,
+	],
+	Area.UNDERGROUND: [
+		0,
+	],
+	Area.CAVERN: [
+		0,
+	],
+	Area.DUNGEON: [
+		0,
+	],
+}
+
+# --- Functions --- #
+func _ready() -> void:
+	# setup cross-fade transition
+	stream.add_transition(
+		AudioStreamInteractive.CLIP_ANY, AudioStreamInteractive.CLIP_ANY,
+		AudioStreamInteractive.TransitionFromTime.TRANSITION_FROM_TIME_IMMEDIATE,
+		AudioStreamInteractive.TransitionToTime.TRANSITION_TO_TIME_START,
+		AudioStreamInteractive.FadeMode.FADE_CROSS, 4
+	)
+	
+	# start playback
+	var args := Globals.parse_arguments()
+	
+	# only play on clients that haven't disabled music
+	if OS.has_feature('dedicated_server') or args.get('server', false) or args.get('no-music', false):
+		return
+
+	play()
+
+## Plays a music track that plays in the given [param area]. Defaults to a random
+## selection from the available options, but can be specified using [param variant]
+func play_track(area: Area, variant := -1) -> void:
+	get_stream_playback().switch_to_clip(AREA_TRACKS[area][variant])
