b63ad251b868b6bac1b0453c9afe4bf3cab1dad7
YLZDC <157612241+YLZDC@users.noreply.github.com>
2026-02-25 19:43:50 -0500

    Hammer Update
    This should add a working hammer.
    
    This should just break WALLS. Not blocks nor trees.
    
    Still need an axe but I think I will do that in another day.

--- items/_resources/swing_item.gd
+++ items/_resources/swing_item.gd
@@ -33,6 +33,21 @@ func handle_interact_mouse(player: PlayerController, mouse_position: Vector2) ->
 		#sends the destroy block function.
 		if not TileManager.destroy_block(tile_position.x, tile_position.y):
 			return
+
+	elif selected.item_id == 9:
+		# checks the range
+		if not is_point_in_range(player, mouse_position):
+			return
+
+		#grabs the tile position
+		var tile_position: Vector2i = TileManager.world_to_tile(
+			floori(mouse_position.x),
+			floori(mouse_position.y)
+		)
+
+		#sends the destroy block function.
+		if not TileManager.destroy_wall(tile_position.x, tile_position.y):
+			return
 	
 
 func simulate_interact_mouse(player: PlayerController, mouse_position: Vector2) -> void:
