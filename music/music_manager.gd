258a18bbe9e4a85753550c7d5f8305ee10c22cee
TheTankManE8 <158609957+TheTankManE8@users.noreply.github.com>
2026-03-03 11:28:21 -0500

    Attempt to add Different Tracks
    Please fix :)

--- music/music_manager.gd
+++ music/music_manager.gd
@@ -1,7 +1,6 @@
 class_name MusicManager
 extends AudioStreamPlayer
 
-# --- Enums --- #
 enum Area {
 	TITLE_SCREEN,
 	FOREST_DAY,
@@ -19,15 +18,15 @@ const AREA_TRACKS: Dictionary[Area, Array] = {
 		0, # res://music/menus/title.ogg
 	],
 	Area.FOREST_DAY: [
-		1, # res://music/forest/forest_day_1.ogg,
-		2, # res://music/forest/forest_day_2.ogg
+		1, # res://music/forest/forest_day_2.ogg
+		5, # res://music/forest/Day Overworld.ogg
 	],
 	Area.FOREST_NIGHT: [
 		0,
 	],
 	Area.WINTER_DAY: [
-		3, # res://music/winter/winter_day_1.ogg,
-		4, # res://music/winter/winter_day_2.ogg
+		2, # res://music/winter/winter_day_1.ogg,
+		3, # res://music/winter/winter_day_2.ogg
 	],
 	Area.WINTER_NIGHT: [
 		0,
@@ -43,9 +42,27 @@ const AREA_TRACKS: Dictionary[Area, Array] = {
 	],
 }
 
+# Areas that use a shuffled queue instead of random selection
+const QUEUED_AREAS: Array[Area] = [
+	Area.FOREST_DAY,
+	Area.FOREST_NIGHT,
+]
+
+# The clip to always play first when entering a queued area.
+# If an area has no entry here, the queue starts immediately.
+const AREA_INTRO_TRACKS: Dictionary[Area, int] = {
+	Area.FOREST_DAY: 1, # res://music/forest/forest_day_1.ogg
+}
+
+var _track_queues: Dictionary[Area, Array] = {}
+var _intro_played: Dictionary[Area, bool] = {}
+var _current_area: Area = Area.TITLE_SCREEN
+var _last_clip: int = -1
+
 # --- Functions --- #
 func _ready() -> void:
 	Globals.music = self
+	finished.connect(_on_track_finished)
 	
 	# setup cross-fade transition
 	stream.add_transition(
@@ -61,17 +78,68 @@ func _ready() -> void:
 	# only play on clients that haven't disabled music
 	if OS.has_feature('dedicated_server') or args.get('server', false) or args.get('no-music', false):
 		return
-
 	play()
 
-## Plays a music track that plays in the given [param area]. Defaults to a random
-## selection from the available options, but can be specified using [param variant]
-func play_track(area: Area, variant := -1) -> void:
-	if multiplayer.is_server() or not is_instance_valid(get_stream_playback() ):
+
+func _process(_delta: float) -> void:
+	if not is_instance_valid(get_stream_playback()):
 		return
 	
-	if variant == -1:
-		variant = randi_range(0, len(AREA_TRACKS[area]))
+	var current_clip: int = get_stream_playback().get_current_clip_index()
 	
+	# detect when a track has finished and moved on
+	if current_clip != _last_clip:
+		_last_clip = current_clip
+	elif not playing:
+		play_track(_current_area)
+
+
+func _on_track_finished() -> void:
+	play_track(_current_area)
+
+
+## Returns the next track index for a queued area, refilling and reshuffling
+## the queue once all tracks have been played.
+func _next_queued_track(area: Area) -> int:
+	# Play the intro track once if one is defined and hasn't been played yet
+	if AREA_INTRO_TRACKS.has(area) and not _intro_played.get(area, false):
+		_intro_played[area] = true
+		return AREA_INTRO_TRACKS[area]
+
+	if not _track_queues.has(area) or _track_queues[area].is_empty():
+		var new_queue: Array = AREA_TRACKS[area].duplicate()
+		# Remove the intro track from the queue so it doesn't repeat
+		if AREA_INTRO_TRACKS.has(area):
+			new_queue.erase(AREA_INTRO_TRACKS[area])
+		new_queue.shuffle()
+		_track_queues[area] = new_queue
+
+	return _track_queues[area].pop_front()
+
+
+## Plays a music track that plays in the given [param area]. Defaults to a random
+## selection from the available options, but can be specified using [param variant].
+## Queued areas (e.g. forest) cycle through all tracks before repeating.
+func play_track(area: Area, variant := -1) -> void:
+	if multiplayer.is_server() or not is_instance_valid(get_stream_playback()):
+		return
+
+	_current_area = area
+
+	var clip: int
+	if variant != -1:
+		clip = AREA_TRACKS[area][variant]
+	elif area in QUEUED_AREAS:
+		clip = _next_queued_track(area)
+	else:
+		clip = AREA_TRACKS[area][randi_range(0, len(AREA_TRACKS[area]) - 1)]
+
 	# fade to new track
-	get_stream_playback().switch_to_clip(AREA_TRACKS[area][variant])
+	get_stream_playback().switch_to_clip(clip)
+
+
+## Resets the queue and intro state for [param area], so the intro plays
+## again next time the player enters it.
+func reset_area(area: Area) -> void:
+	_intro_played.erase(area)
+	_track_queues.erase(area)
