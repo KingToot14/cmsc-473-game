050db1fe6a72bdc4f41dd9eb4db3f20e7cb10024
Jacob Vanluven <46078617+KingToot14@users.noreply.github.com>
2026-03-03 23:36:54 -0500

    Added Custom Cursors
    Added the infrastructure for setting custom cursors based on a few presets. Also implemented a system to automatically re-size the cursor based on viewport size.

--- scripts/autoloads/globals.gd
+++ scripts/autoloads/globals.gd
@@ -3,8 +3,21 @@ extends Node
 # -- Signals --- #
 signal world_size_changed(size: Vector2i)
 
+# --- Enums --- #
+enum CursorType {
+	ARROW,
+	BEAM,
+	CROSS,
+	PICKAXE,
+	AXE,
+	HAMMER,
+	OPEN
+}
+
 # --- Variables --- #
 const SERVER_ID := 1
+
+const CURSOR_SIZE := 12
 
 var world_size := Vector2i(4200, 1200):
 	set(_size):
@@ -36,9 +49,17 @@ var music: MusicManager
 # - Items
 var item_registry: Dictionary[int, String] = {}
 
+# - Cursor
+var current_cursor := CursorType.ARROW
+var scale_factor := 1.0
+
 # --- Functions --- #
 func _ready() -> void:
 	world_size = world_size
+	
+	get_viewport().size_changed.connect(_on_viewport_size_changed)
+	
+	reset_cursors()
 
 func parse_arguments() -> Dictionary:
 	var arguments: Dictionary = {}
@@ -52,3 +73,70 @@ func parse_arguments() -> Dictionary:
 			arguments[arg] = true
 	
 	return arguments
+
+#region Cursors
+func _on_viewport_size_changed() -> void:
+	var scale := get_viewport().get_stretch_transform().get_scale()
+	scale_factor = min(scale.x, scale.y)
+	
+	reset_cursors()
+
+func reset_cursors() -> void:
+	# set beam cursor
+	var beam_image := get_cursor_image(1, 0)
+	Input.set_custom_mouse_cursor(beam_image, Input.CURSOR_IBEAM, Vector2(CURSOR_SIZE, CURSOR_SIZE))
+	
+	# reload current cursor
+	set_cursor(current_cursor)
+
+func set_cursor(type: CursorType) -> void:
+	var cursor_image: Image
+	var hotspot := Vector2(CURSOR_SIZE / 2.0, CURSOR_SIZE / 2.0)
+	
+	current_cursor = type
+	
+	# set image and hotspots
+	match type:
+		CursorType.ARROW:
+			cursor_image = get_cursor_image(0, 0)
+			hotspot = Vector2(2, 1)
+		CursorType.BEAM:
+			cursor_image = get_cursor_image(1, 0)
+		CursorType.CROSS:
+			cursor_image = get_cursor_image(2, 0)
+		CursorType.PICKAXE:
+			cursor_image = get_cursor_image(0, 1)
+			hotspot = Vector2(1, 1)
+		CursorType.AXE:
+			cursor_image = get_cursor_image(1, 1)
+			hotspot = Vector2(1, 1)
+		CursorType.HAMMER:
+			cursor_image = get_cursor_image(2, 1)
+			hotspot = Vector2(1, 1)
+		CursorType.OPEN:
+			cursor_image = get_cursor_image(0, 2)
+			hotspot = Vector2(1, 1)
+	
+	# set custom mouse
+	Input.set_custom_mouse_cursor(
+		cursor_image,
+		Input.CURSOR_ARROW,
+		Vector2(floori(hotspot.x * scale_factor), floori(hotspot.y * scale_factor))
+	)
+
+func get_cursor_image(x: int, y: int) -> Image:
+	var cursor_texture := AtlasTexture.new()
+	cursor_texture.atlas = preload("res://ui/cursors.png")
+	cursor_texture.region = Rect2(x * CURSOR_SIZE, y * CURSOR_SIZE, CURSOR_SIZE, CURSOR_SIZE)
+	
+	# resize to fit viewport
+	var image := cursor_texture.get_image()
+	image.resize(
+		floori(CURSOR_SIZE * scale_factor),
+		floori(CURSOR_SIZE * scale_factor),
+		Image.INTERPOLATE_NEAREST
+	)
+	
+	return image
+
+#endregion
