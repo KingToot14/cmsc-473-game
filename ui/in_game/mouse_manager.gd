d8f4e6d01bd4267c45f101930e664de296f1b9cf
Jacob Vanluven <46078617+KingToot14@users.noreply.github.com>
2026-03-04 10:06:24 -0500

    Tied Interaction to Mouse Detector
    Changed how mouse input works from being global to using _gui_input in the mouse_detector behind the inventory. This means that we no longer use tools when interacting in the inventory.

--- ui/in_game/mouse_manager.gd
+++ ui/in_game/mouse_manager.gd
@@ -14,3 +14,27 @@ func _process(delta: float) -> void:
 	)
 	
 	RenderingServer.global_shader_parameter_set(&"mouse_position", player.get_global_mouse_position())
+
+func _gui_input(event: InputEvent) -> void:
+	# calculate global mouse position
+	var mouse_position := get_global_mouse_position()
+	var tile_position := TileManager.world_to_tile(floori(mouse_position.x), floori(mouse_position.y))
+	
+	# only used for client-side interaction
+	if event.is_action_pressed(&'interact'):
+		if Globals.hovered_hitbox and Globals.hovered_hitbox.entity.interact_with(tile_position):
+			return
+	if event.is_action_pressed(&'break_place'):
+		# check if player can act
+		if not player.can_act():
+			return
+		
+		# check if hotbar item is empty
+		var item_stack: Inventory.ItemStack = player.my_inventory.get_selected_item()
+		if item_stack.is_empty():
+			return
+		
+		# check if item exists
+		var item: Item = ItemDatabase.get_item(item_stack.item_id)
+		if item:
+			item.handle_interact_mouse(player, mouse_position)
