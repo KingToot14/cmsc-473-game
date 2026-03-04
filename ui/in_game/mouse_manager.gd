44d9a069cd6a81645ead933ef88e16fe31873997
Jacob Vanluven <46078617+KingToot14@users.noreply.github.com>
2026-03-04 14:57:21 -0500

    Added Item Cursors
    Added custom cursors for tool and block items

--- ui/in_game/mouse_manager.gd
+++ ui/in_game/mouse_manager.gd
@@ -8,8 +8,14 @@ extends Control
 
 var current_item: Item
 
+var cursor_locked := false
+
 # --- Functions --- #
-func _process(delta: float) -> void:
+func _ready() -> void:
+	if player.owner_id == multiplayer.get_unique_id():
+		Globals.mouse = self
+
+func _process(_delta: float) -> void:
 	# update grid overlay position (snap to actual tile grid)
 	grid_overlay.global_position = Vector2(
 		floorf((player.global_position.x - grid_overlay.size.x / 2.0) / 8.0) * 8.0,
@@ -17,11 +23,46 @@ func _process(delta: float) -> void:
 	)
 	
 	# update shader mouse position
-	RenderingServer.global_shader_parameter_set(&"mouse_position", player.get_global_mouse_position())
+	var mouse_pos := player.get_global_mouse_position()
+	
+	RenderingServer.global_shader_parameter_set(&"mouse_position", mouse_pos)
 	
 	# process item
 	if current_item:
-		current_item.handle_process(player, player.get_global_mouse_position())
+		current_item.handle_process(player, mouse_pos)
+	
+	# update mouse type
+	if cursor_locked:
+		return
+	
+	var item := ItemDatabase.get_item(player.my_inventory.get_selected_item().item_id)
+	
+	# try to match tool
+	if item and item is ToolItem:
+		# load pickaxe cursor
+		if item.tool_type == ToolItem.ToolType.PICKAXE:
+			Globals.set_cursor(Globals.CursorType.PICKAXE)
+			return
+		# load axe cursor
+		elif item.tool_type == ToolItem.ToolType.AXE:
+			Globals.set_cursor(Globals.CursorType.AXE)
+			return
+		# load hammer cursor
+		elif item.tool_type == ToolItem.ToolType.HAMMER:
+			Globals.set_cursor(Globals.CursorType.HAMMER)
+			return
+	# try to match block
+	elif item and item is BlockItem:
+		# load block cursor
+		if item.tile_type == BlockItem.TileType.BLOCK:
+			Globals.set_cursor(Globals.CursorType.BLOCK)
+			return
+		# load wall cursor
+		elif item.tile_type == BlockItem.TileType.WALL:
+			Globals.set_cursor(Globals.CursorType.WALL)
+			return
+	
+	Globals.set_cursor(Globals.CursorType.ARROW)
 
 func _gui_input(event: InputEvent) -> void:
 	# calculate global mouse position
