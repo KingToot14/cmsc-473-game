b898d2968848bbf1f91db7728239205ef0e984c8
Jacob Vanluven <46078617+KingToot14@users.noreply.github.com>
2026-03-30 11:40:42 -0400

    Added Armor Sprite Support
    Added support for armor sprites. Currently the sprites aren't finished, but they do exist and should work going forward.

---
+++ entities/player/outfits/armor_set.gd
@@ -0,0 +1,12 @@
+class_name ArmorSet
+extends Resource
+
+# --- Variables --- #
+@export var arm_back_sprite: Texture2D
+@export var leg_back_sprite: Texture2D
+@export var body_sprite: Texture2D
+@export var leg_front_sprite: Texture2D
+@export var head_sprite: Texture2D
+@export var arm_front_sprite: Texture2D
+
+# --- Functions --- #
