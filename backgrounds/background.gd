94407e18f7d9476f8035e55f34180ebadc299152
Jacob Vanluven <46078617+KingToot14@users.noreply.github.com>
2026-04-01 10:18:49 -0400

    Added background sky color tweening
    The background color can now be tweened based on the daytime value

--- backgrounds/background.gd
+++ backgrounds/background.gd
@@ -14,7 +14,31 @@ var tween: Tween
 	'forest', 'desert', 'snow', 'ocean'
 ) var allowed_biomes := 0
 
+@export var sky_shader: ShaderMaterial
+@export var sky_gradient_top: GradientTexture1D
+@export var sky_gradient_bottom: GradientTexture1D
+
 # --- Functions --- #
+func _ready() -> void:
+	if OS.has_feature('dedicated_server') or sky_shader == null:
+		set_process(false)
+
+func _process(_delta: float) -> void:
+	if not visible:
+		return
+	
+	# update sky
+	var gradient_pos := DaytimeManager.curr_time_percent * 960
+	
+	sky_shader.set_shader_parameter(
+		&'top_color',
+		sky_gradient_top.get_image().get_pixel(floori(gradient_pos), 0)
+	)
+	sky_shader.set_shader_parameter(
+		&'bottom_color',
+		sky_gradient_bottom.get_image().get_pixel(floori(gradient_pos), 0)
+	)
+
 func update_background(biome: BiomeManager.Biome, layer: BiomeManager.Layer) -> void:
 	if (biome & allowed_biomes) and (layer & allowed_layers):
 		if tween:
