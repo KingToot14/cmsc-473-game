39d804a55795b911a320aba7586c41f4e800e900
Jacob Vanluven <46078617+KingToot14@users.noreply.github.com>
2026-04-02 09:09:08 -0400

    Added Armor Slot Icons

--- entities/player/armor_slot.gd
+++ entities/player/armor_slot.gd
@@ -1,7 +1,20 @@
 class_name ArmorSlot
 extends InventorySlot
 
-@export var expected_type: ArmorItem.ArmorType
+# --- Variables
+@export var slot_atlas: AtlasTexture
+
+@export var expected_type: ArmorItem.ArmorType:
+	set(_type):
+		expected_type = _type
+		
+		match _type:
+			ArmorItem.ArmorType.HEAD:
+				slot_atlas.region = Rect2i(0, 0, 16, 16)
+			ArmorItem.ArmorType.BODY:
+				slot_atlas.region = Rect2i(16, 0, 16, 16)
+			ArmorItem.ArmorType.LEGS:
+				slot_atlas.region = Rect2i(0, 16, 16, 16)
 
 func _gui_input(event: InputEvent) -> void:
 	if is_display_only:
