63cf1e8c6084ad2a69454f99e51a6c89b804b82c
RandomizedNonsense <rskuhns42@gmail.com>
2026-04-14 21:05:18 -0400

    Crafting Bench Works
    Crafting bench should work, we will need to make more recipes but for now wooden tools are in the crafting bench rather than the basic crafting menu.

--- entities/tile_entities/9_crafting_station/crafting_bench_entity.gd
+++ entities/tile_entities/9_crafting_station/crafting_bench_entity.gd
@@ -1,4 +1,4 @@
-class_name CraftingStationEntity
+class_name CraftingBenchEntity
 extends TileEntity
 
 # --- Enums --- #
@@ -10,6 +10,7 @@ enum CraftingVariant {
 
 # --- Variables --- #
 var variant := CraftingVariant.OAK
+var in_use_by := 0
 
 @export var variant_sprites: Dictionary[CraftingVariant, Texture2D] = {}
 
@@ -17,7 +18,6 @@ var variant := CraftingVariant.OAK
 #region Sprite
 func setup_variant() -> void:
 	$'sprite'.texture = variant_sprites[variant]
-
 #endregion
 
 #region Interaction
@@ -28,9 +28,53 @@ func interact_with(mouse_position: Vector2) -> bool:
 	if not Globals.player.is_point_in_range(mouse_position):
 		return false
 	
-	# do crafting stuff
+	# Ask the server for permission to lock/open this bench
+	if multiplayer.is_server():
+		request_open_bench()
+	else:
+		request_open_bench.rpc_id(Globals.SERVER_ID)
 	
 	return true
+
+@rpc('any_peer', 'call_remote', 'reliable')
+func request_open_bench() -> void:
+	var sender_id = multiplayer.get_remote_sender_id()
+	if sender_id == 0: 
+		sender_id = 1 
+	
+	if in_use_by == 0 or in_use_by == sender_id:
+		in_use_by = sender_id
+		
+		if sender_id == 1:
+			open_bench_client()
+		else:
+			open_bench_client.rpc_id(sender_id)
+
+@rpc('authority', 'call_remote', 'reliable')
+func open_bench_client() -> void:
+	var bench_ui = Globals.player.get_node_or_null("inventory_ui/crafting_bench_container")
+	if bench_ui:
+		bench_ui.open_crafting_bench(self)
+
+@rpc('any_peer', 'call_remote', 'reliable')
+func release_bench() -> void:
+	var sender_id = multiplayer.get_remote_sender_id()
+	if sender_id == 0:
+		sender_id = 1
+	
+	if in_use_by == sender_id:
+		in_use_by = 0
+
+func spawn_item() -> void:
+	# Drop the crafting bench item (Item ID 100)
+	var drop_pos = global_position + Vector2(8, 8) 
+	ItemDropEntity.spawn(drop_pos, 100, 1)
+	
+	destroy_bench.rpc()
+
+@rpc('authority', 'call_local', 'reliable')
+func destroy_bench() -> void:
+	queue_free()
 
 #endregion
 
@@ -56,12 +100,10 @@ func deserialize_spawn_data(buffer: StreamPeerBuffer) -> void:
 	variant = buffer.get_u16() as CraftingVariant
 	
 	setup_variant()
-
 #endregion
 
 #region Spawning
 static func create(tile_pos: Vector2i, tile_variant := &'normal') -> void:
-	# create new tree entity
 	var entity_scene: PackedScene = EntityManager.tile_entity_registry.get(9).entity_scene
 	if not entity_scene:
 		return
@@ -70,14 +112,14 @@ static func create(tile_pos: Vector2i, tile_variant := &'normal') -> void:
 	if not is_placement_valid(tile_pos):
 		return
 	
-	var entity: CraftingStationEntity = entity_scene.instantiate()
+	var entity: CraftingBenchEntity = entity_scene.instantiate()
 	
 	# setup default parameters
 	entity.tile_position = tile_pos
 	entity.global_position = TileManager.tile_to_world(tile_pos.x, tile_pos.y)
 	
 	match tile_variant:
-		&'oak':
+		&'oak', &'normal':
 			entity.variant = CraftingVariant.OAK
 		&'spruce':
 			entity.variant = CraftingVariant.SPRUCE
@@ -87,12 +129,8 @@ static func create(tile_pos: Vector2i, tile_variant := &'normal') -> void:
 	entity.setup_variant()
 	
 	EntityManager.store_tile_entity(9, entity)
-	
 	return
 
-## Returns whether or not the placement is valid. This uses
-## [method get_collision_grid] to check for collision and 
-## [method get_variant] to make sure the ground beneath is valid.
 static func is_placement_valid(tile_pos: Vector2i) -> bool:
 	# make sure bottom tiles are filled
 	for x in range(2):
@@ -108,10 +146,6 @@ static func is_placement_valid(tile_pos: Vector2i) -> bool:
 	
 	return true
 
-## Returns a collision grid representing obstacles that are in the way of placement.
-## The results is a [code]Dictionary[lb]Vector2i, bool[rb][/code] that maps
-## tile positions to whether or not a collision was detected (either a block or
-## another tile entity)
 static func get_collision_grid(tile_pos: Vector2i) -> Dictionary[Vector2i, bool]:
 	var grid: Dictionary[Vector2i, bool] = {}
 	
@@ -126,5 +160,4 @@ static func get_collision_grid(tile_pos: Vector2i) -> Dictionary[Vector2i, bool]
 			grid[pos] = true
 
 	return grid
-
 #endregion
